98617d6739618599062e91cc6ce834d88c676542
tobias wolf <tobias.wolf@diawa.at>
2023-01-04 15:39:03 +0100

    Adds DbMerger tool

---
+++ tools/dbmerger/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace DbMerger;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0 || 
+            args.Contains("-h"))
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("-h Help");
+            Console.WriteLine("-s Source database file path");
+            Console.WriteLine("-d Target database file path");
+        }
+
+        string? source = null;
+        string? destination = null;
+
+        if (args.Contains("-s"))
+        {
+            source = args[Array.IndexOf(args, "-s") + 1];
+            
+            // Check if source is a valid path to a file
+            var sourceFile = new FileInfo(source);
+            
+            if (!sourceFile.Exists)
+            {
+                Console.WriteLine("Source file does not exist");
+                return;
+            }
+        }
+
+        if (args.Contains("-d"))
+        {
+            destination = args[Array.IndexOf(args, "-d") + 1];
+            
+            var destinationFile = new FileInfo(destination);
+            
+            if (!destinationFile.Exists)
+            {
+                Console.WriteLine("Destination file does not exist. Do you wish to create it? (y/n)");
+                if (!destinationFile.Exists)
+                {
+                    // Create a new sqlite database file
+                    SQLiteConnection.CreateFile(destination);
+                    
+                    // Open the connection
+                    using var connection = new SQLiteConnection($"Data Source={destination};Version=3;");
+                    connection.Open();
+                    
+                    // Execute some sql
+                    using var createTableCode = new SQLiteCommand(connection);
+                    createTableCode.CommandText = "CREATE TABLE IF NOT EXISTS \"code\" (\n\t\"id\"\tINTEGER,\n\t\"language\"\tTEXT NOT NULL,\n\t\"url\"\tTEXT NOT NULL,\n\t\"content\"\tTEXT NOT NULL,\n\t\"hash\"\tTEXT NOT NULL UNIQUE,\n\t\"size\"\tINTEGER NOT NULL DEFAULT 0,\n\tPRIMARY KEY(\"id\" AUTOINCREMENT)\n);";
+                    
+                    createTableCode.ExecuteNonQuery();
+
+                    using var createTableProgress = new SQLiteCommand(connection);
+                    createTableProgress.CommandText = "CREATE TABLE IF NOT EXISTS \"progress\" (\n    \t\"language\"\tTEXT NOT NULL,\n    \t\"query\"\tTEXT NOT NULL,\n    \t\"last_page\"\tINTEGER NOT NULL DEFAULT 0,\n    \tPRIMARY KEY(\"language\", \"query\")\n);";
+
+                    createTableProgress.ExecuteNonQuery();
+                    
+                    connection.Close();
+                }
+                
+                // var answer = Console.ReadLine();
+                //
+                // if (answer == "y")
+                // {
+                //     destinationFile.Create();
+                // }
+                // else
+                // {
+                //     Console.WriteLine("Aborting.");
+                //     return;
+                // }
+            }
+        }
+
+
+        if (string.IsNullOrWhiteSpace(source) ||
+            string.IsNullOrWhiteSpace(destination))
+        {
+            Console.WriteLine("Please provide source and destination file paths");
+        }
+
+
+        var foo = new SQLiteDbMerger();
+        foo.Merge(source, destination);
+
+
+
+        return;
+    }
+}
+
---
+++ tools/dbmerger/SQLiteDbMerger.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Data.SQLite;
+
+namespace DbMerger;
+
+public class SQLiteDbMerger
+{
+    public void Merge(string sourceDbPath, string targetDbPath)
+    {
+        using var sourceDb = new SQLiteConnection("Data Source=" + sourceDbPath);
+        using var targetDb = new SQLiteConnection("Data Source=" + targetDbPath);
+        
+        sourceDb.Open();
+        targetDb.Open();
+        
+        // Copy over all data from sourceDb to targetDb
+        var tables = new[] { "code", "progress" };
+        
+        // Code
+        using var cmd = new SQLiteCommand("SELECT * FROM code", sourceDb);
+        using var reader = cmd.ExecuteReader();
+        while (reader.Read())
+        {
+            
+            var insertCmd = new SQLiteCommand("INSERT INTO code (language, url, content, hash, size) VALUES (?, ?, ?, ?, ?)", targetDb);
+            for (int i = 1; i < reader.FieldCount; i++)
+            {
+                insertCmd.Parameters.AddWithValue("@" + i, reader[i]);
+            }
+            
+            // Try executing the command
+            try
+            {
+                insertCmd.ExecuteNonQuery();
+            }
+            catch (SQLiteException e)
+            {
+                // Ignore duplicate key errors
+                if (e.ErrorCode != 19)
+                {
+                    throw;
+                }
+            }
+        }
+        
+        // Progress
+        using var cmd2 = new SQLiteCommand("SELECT * FROM progress", sourceDb);
+        using var reader2 = cmd2.ExecuteReader();
+        while (reader2.Read())
+        {
+            var insertOrReplaceCmd = new SQLiteCommand("INSERT OR REPLACE INTO progress (language, query, last_page) VALUES (?, ?, ?)", targetDb);
+            for (int i = 0; i < reader2.FieldCount; i++)
+            {
+                insertOrReplaceCmd.Parameters.AddWithValue("@" + i, reader2[i]);
+            }
+            
+            insertOrReplaceCmd.ExecuteNonQuery();
+        }
+    }
+}
